97802c0dee7496f77491992c44d91ffe228e6a83
tboy4all <taiyesuleiman09@gmail.com>
2023-01-05 11:36:14 +0100

    updated package.json

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const Stripe = require('stripe');
-// import { loadStripe } from '@stripe/stripe-js';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const Stripe = require('stripe');
 // const stripe = Stripe(
 //   'pk_test_51MK2c1KaCu9yH5TV2sS5fXxFJUZA8uULB5VN6CFWhxZs50xxvOAmw3sy6wpP8N9ZXyz0UCUFKff0SoPEZ6oMkcgc0024lvA8dD'
 // );
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const sessionResponse = await axios.get(
+    const sessionResponse = await axios(
       // `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
       `/api/v1/bookings/checkout-session/${tourId}` // for production
     );
